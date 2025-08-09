581ca9c8722291d90f793e111189f0d2aa644cb0
Prem-Bhugra <ch7221038@iitd.ac.in>
2025-08-09 19:03:07 +0530

    Tutorial 7- hyperparameter training

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -11,9 +11,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 from src.components.data_transformation import DataTransformation
-from src.components.data_transformation import DataTransformationConfig
 from src.components.model_trainer import ModelTrainer
-from src.components.model_trainer import ModelTrainerConfig
 
 @dataclass
 class DataIngestionConfig:
@@ -50,5 +48,5 @@ if __name__=="__main__":
     data_transformation = DataTransformation()
     train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data, test_data)
     model_trainer= ModelTrainer()
-    print(model_trainer.initiatie_model_trainer(train_arr,test_arr))
+    print(model_trainer.initiate_model_trainer(train_arr,test_arr))
 
--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,7 @@ import pandas as pd
 from src.exception import CustomException
 import dill
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 def save_object(file_path, obj):
     try:
@@ -15,12 +16,16 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
     
-def evaluate_models(x_train, y_train, x_test, y_test, models):
+def evaluate_models(x_train, y_train, x_test, y_test, models,params):
     try:
         report={}
         for i in range(len(list(models))):
             model=list(models.values())[i]
-            model.fit(x_train,y_train) #Training the model
+            param=params[list(models.keys())[i]]
+            gs = GridSearchCV(model,param,cv=3)
+            gs.fit(x_train,y_train)
+            model.set_params(**gs.best_params_)
+            model.fit(x_train, y_train)
             y_test_pred=model.predict(x_test)
             test_model_score=r2_score(y_test, y_test_pred)
             report[list(models.keys())[i]] = test_model_score
